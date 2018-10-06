cce480da8511725e9d9724691779d7a7a0647a46
Eliott Salazar <eliottsalazar@gmail.com>
2018-10-05 18:58:36 -0700

    Avances del dia 05/10/2018

--- Estructura_De_Datos_Proyecto/Estructura_De_Datos_Proyecto/frmSumaDeMatrices.Designer.cs
+++ Estructura_De_Datos_Proyecto/Estructura_De_Datos_Proyecto/frmSumaDeMatrices.Designer.cs
@@ -29,20 +29,27 @@
         private void InitializeComponent()
         {
             this.pnlTitulo = new System.Windows.Forms.Panel();
-            this.lblTitulo = new System.Windows.Forms.Label();
             this.pbxCerrar = new System.Windows.Forms.PictureBox();
+            this.lblTitulo = new System.Windows.Forms.Label();
             this.pnlContenido = new System.Windows.Forms.Panel();
-            this.dgvMatriz1 = new System.Windows.Forms.DataGridView();
-            this.btnRandom = new System.Windows.Forms.Button();
-            this.btnSuma = new System.Windows.Forms.Button();
-            this.dgvMatriz2 = new System.Windows.Forms.DataGridView();
             this.dgvMatrizResultado = new System.Windows.Forms.DataGridView();
+            this.dgvMatriz2 = new System.Windows.Forms.DataGridView();
+            this.btnSuma = new System.Windows.Forms.Button();
+            this.btnRandom = new System.Windows.Forms.Button();
+            this.dgvMatriz1 = new System.Windows.Forms.DataGridView();
+            this.lblMas = new System.Windows.Forms.Label();
+            this.pnlCampos = new System.Windows.Forms.Panel();
+            this.lblValor1 = new System.Windows.Forms.Label();
+            this.txtLimiteInferior = new System.Windows.Forms.TextBox();
+            this.lblLimiteSuperior = new System.Windows.Forms.Label();
+            this.txtLimiteSuperior = new System.Windows.Forms.TextBox();
             this.pnlTitulo.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbxCerrar)).BeginInit();
             this.pnlContenido.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvMatriz1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvMatriz2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvMatrizResultado)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMatriz2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMatriz1)).BeginInit();
+            this.pnlCampos.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnlTitulo
@@ -55,6 +62,18 @@
             this.pnlTitulo.Name = "pnlTitulo";
             this.pnlTitulo.Size = new System.Drawing.Size(852, 36);
             this.pnlTitulo.TabIndex = 0;
+            // 
+            // pbxCerrar
+            // 
+            this.pbxCerrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbxCerrar.BackgroundImage = global::Estructura_De_Datos_Proyecto.Properties.Resources.Windows_Close_Program_22531;
+            this.pbxCerrar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbxCerrar.Location = new System.Drawing.Point(816, 0);
+            this.pbxCerrar.Name = "pbxCerrar";
+            this.pbxCerrar.Size = new System.Drawing.Size(36, 36);
+            this.pbxCerrar.TabIndex = 1;
+            this.pbxCerrar.TabStop = false;
+            this.pbxCerrar.Click += new System.EventHandler(this.pbxCerrar_Click);
             // 
             // lblTitulo
             // 
@@ -69,25 +88,14 @@
             this.lblTitulo.TabIndex = 0;
             this.lblTitulo.Text = "Suma de matrices";
             // 
-            // pbxCerrar
-            // 
-            this.pbxCerrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.pbxCerrar.BackgroundImage = global::Estructura_De_Datos_Proyecto.Properties.Resources.Windows_Close_Program_22531;
-            this.pbxCerrar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pbxCerrar.Location = new System.Drawing.Point(816, 0);
-            this.pbxCerrar.Name = "pbxCerrar";
-            this.pbxCerrar.Size = new System.Drawing.Size(36, 36);
-            this.pbxCerrar.TabIndex = 1;
-            this.pbxCerrar.TabStop = false;
-            this.pbxCerrar.Click += new System.EventHandler(this.pbxCerrar_Click);
-            // 
             // pnlContenido
             // 
             this.pnlContenido.BackColor = System.Drawing.Color.Silver;
+            this.pnlContenido.Controls.Add(this.pnlCampos);
+            this.pnlContenido.Controls.Add(this.lblMas);
             this.pnlContenido.Controls.Add(this.dgvMatrizResultado);
             this.pnlContenido.Controls.Add(this.dgvMatriz2);
             this.pnlContenido.Controls.Add(this.btnSuma);
-            this.pnlContenido.Controls.Add(this.btnRandom);
             this.pnlContenido.Controls.Add(this.dgvMatriz1);
             this.pnlContenido.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pnlContenido.Location = new System.Drawing.Point(0, 36);
@@ -95,29 +103,36 @@
             this.pnlContenido.Size = new System.Drawing.Size(852, 426);
             this.pnlContenido.TabIndex = 1;
             // 
-            // dgvMatriz1
+            // dgvMatrizResultado
             // 
-            this.dgvMatriz1.AllowUserToAddRows = false;
-            this.dgvMatriz1.AllowUserToDeleteRows = false;
-            this.dgvMatriz1.AllowUserToResizeColumns = false;
-            this.dgvMatriz1.AllowUserToResizeRows = false;
-            this.dgvMatriz1.BackgroundColor = System.Drawing.Color.White;
-            this.dgvMatriz1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvMatriz1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.dgvMatriz1.Location = new System.Drawing.Point(0, 0);
-            this.dgvMatriz1.Name = "dgvMatriz1";
-            this.dgvMatriz1.RowHeadersVisible = false;
-            this.dgvMatriz1.Size = new System.Drawing.Size(240, 426);
-            this.dgvMatriz1.TabIndex = 0;
+            this.dgvMatrizResultado.AllowUserToAddRows = false;
+            this.dgvMatrizResultado.AllowUserToDeleteRows = false;
+            this.dgvMatrizResultado.AllowUserToResizeColumns = false;
+            this.dgvMatrizResultado.AllowUserToResizeRows = false;
+            this.dgvMatrizResultado.BackgroundColor = System.Drawing.Color.White;
+            this.dgvMatrizResultado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMatrizResultado.Location = new System.Drawing.Point(612, 0);
+            this.dgvMatrizResultado.Name = "dgvMatrizResultado";
+            this.dgvMatrizResultado.ReadOnly = true;
+            this.dgvMatrizResultado.RowHeadersVisible = false;
+            this.dgvMatrizResultado.Size = new System.Drawing.Size(240, 380);
+            this.dgvMatrizResultado.TabIndex = 4;
             // 
-            // btnRandom
+            // dgvMatriz2
             // 
-            this.btnRandom.Location = new System.Drawing.Point(104, 18);
-            this.btnRandom.Name = "btnRandom";
-            this.btnRandom.Size = new System.Drawing.Size(96, 34);
-            this.btnRandom.TabIndex = 1;
-            this.btnRandom.Text = "Random";
-            this.btnRandom.UseVisualStyleBackColor = true;
+            this.dgvMatriz2.AllowUserToAddRows = false;
+            this.dgvMatriz2.AllowUserToDeleteRows = false;
+            this.dgvMatriz2.AllowUserToResizeColumns = false;
+            this.dgvMatriz2.AllowUserToResizeRows = false;
+            this.dgvMatriz2.BackgroundColor = System.Drawing.Color.White;
+            this.dgvMatriz2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMatriz2.Location = new System.Drawing.Point(300, -3);
+            this.dgvMatriz2.Name = "dgvMatriz2";
+            this.dgvMatriz2.RowHeadersVisible = false;
+            this.dgvMatriz2.Size = new System.Drawing.Size(258, 383);
+            this.dgvMatriz2.TabIndex = 3;
+            this.dgvMatriz2.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvMatriz2_CellEndEdit);
+            this.dgvMatriz2.CellLeave += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvMatriz2_CellLeave);
             // 
             // btnSuma
             // 
@@ -131,36 +146,99 @@
             this.btnSuma.TabIndex = 2;
             this.btnSuma.Text = "=";
             this.btnSuma.UseVisualStyleBackColor = false;
+            this.btnSuma.Click += new System.EventHandler(this.btnSuma_Click);
             // 
-            // dgvMatriz2
+            // btnRandom
             // 
-            this.dgvMatriz2.AllowUserToAddRows = false;
-            this.dgvMatriz2.AllowUserToDeleteRows = false;
-            this.dgvMatriz2.AllowUserToResizeColumns = false;
-            this.dgvMatriz2.AllowUserToResizeRows = false;
-            this.dgvMatriz2.BackgroundColor = System.Drawing.Color.White;
-            this.dgvMatriz2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvMatriz2.Location = new System.Drawing.Point(300, 0);
-            this.dgvMatriz2.Name = "dgvMatriz2";
-            this.dgvMatriz2.RowHeadersVisible = false;
-            this.dgvMatriz2.Size = new System.Drawing.Size(258, 426);
-            this.dgvMatriz2.TabIndex = 3;
+            this.btnRandom.BackColor = System.Drawing.Color.White;
+            this.btnRandom.FlatAppearance.BorderSize = 0;
+            this.btnRandom.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRandom.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRandom.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnRandom.Location = new System.Drawing.Point(541, 5);
+            this.btnRandom.Name = "btnRandom";
+            this.btnRandom.Size = new System.Drawing.Size(206, 37);
+            this.btnRandom.TabIndex = 1;
+            this.btnRandom.Text = "Random";
+            this.btnRandom.UseVisualStyleBackColor = false;
+            this.btnRandom.Click += new System.EventHandler(this.btnRandom_Click);
             // 
-            // dgvMatrizResultado
+            // dgvMatriz1
             // 
-            this.dgvMatrizResultado.AllowUserToAddRows = false;
-            this.dgvMatrizResultado.AllowUserToDeleteRows = false;
-            this.dgvMatrizResultado.AllowUserToResizeColumns = false;
-            this.dgvMatrizResultado.AllowUserToResizeRows = false;
-            this.dgvMatrizResultado.BackgroundColor = System.Drawing.Color.White;
-            this.dgvMatrizResultado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvMatrizResultado.Dock = System.Windows.Forms.DockStyle.Right;
-            this.dgvMatrizResultado.Location = new System.Drawing.Point(612, 0);
-            this.dgvMatrizResultado.Name = "dgvMatrizResultado";
-            this.dgvMatrizResultado.ReadOnly = true;
-            this.dgvMatrizResultado.RowHeadersVisible = false;
-            this.dgvMatrizResultado.Size = new System.Drawing.Size(240, 426);
-            this.dgvMatrizResultado.TabIndex = 4;
+            this.dgvMatriz1.AllowUserToAddRows = false;
+            this.dgvMatriz1.AllowUserToDeleteRows = false;
+            this.dgvMatriz1.AllowUserToResizeColumns = false;
+            this.dgvMatriz1.AllowUserToResizeRows = false;
+            this.dgvMatriz1.BackgroundColor = System.Drawing.Color.White;
+            this.dgvMatriz1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMatriz1.Location = new System.Drawing.Point(0, 0);
+            this.dgvMatriz1.Name = "dgvMatriz1";
+            this.dgvMatriz1.RowHeadersVisible = false;
+            this.dgvMatriz1.Size = new System.Drawing.Size(240, 380);
+            this.dgvMatriz1.TabIndex = 0;
+            // 
+            // lblMas
+            // 
+            this.lblMas.AutoSize = true;
+            this.lblMas.BackColor = System.Drawing.Color.Transparent;
+            this.lblMas.Font = new System.Drawing.Font("Century Gothic", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMas.Location = new System.Drawing.Point(254, 195);
+            this.lblMas.Name = "lblMas";
+            this.lblMas.Size = new System.Drawing.Size(32, 36);
+            this.lblMas.TabIndex = 5;
+            this.lblMas.Text = "+";
+            // 
+            // pnlCampos
+            // 
+            this.pnlCampos.BackColor = System.Drawing.Color.Black;
+            this.pnlCampos.Controls.Add(this.txtLimiteSuperior);
+            this.pnlCampos.Controls.Add(this.lblLimiteSuperior);
+            this.pnlCampos.Controls.Add(this.btnRandom);
+            this.pnlCampos.Controls.Add(this.txtLimiteInferior);
+            this.pnlCampos.Controls.Add(this.lblValor1);
+            this.pnlCampos.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlCampos.Location = new System.Drawing.Point(0, 380);
+            this.pnlCampos.Name = "pnlCampos";
+            this.pnlCampos.Size = new System.Drawing.Size(852, 46);
+            this.pnlCampos.TabIndex = 6;
+            // 
+            // lblValor1
+            // 
+            this.lblValor1.AutoSize = true;
+            this.lblValor1.ForeColor = System.Drawing.Color.White;
+            this.lblValor1.Location = new System.Drawing.Point(12, 11);
+            this.lblValor1.Name = "lblValor1";
+            this.lblValor1.Size = new System.Drawing.Size(118, 21);
+            this.lblValor1.TabIndex = 0;
+            this.lblValor1.Text = "Límite inferior: ";
+            // 
+            // txtLimiteInferior
+            // 
+            this.txtLimiteInferior.Location = new System.Drawing.Point(138, 8);
+            this.txtLimiteInferior.MaxLength = 10;
+            this.txtLimiteInferior.Name = "txtLimiteInferior";
+            this.txtLimiteInferior.Size = new System.Drawing.Size(100, 27);
+            this.txtLimiteInferior.TabIndex = 1;
+            this.txtLimiteInferior.TextChanged += new System.EventHandler(this.txtLimiteInferior_TextChanged);
+            // 
+            // lblLimiteSuperior
+            // 
+            this.lblLimiteSuperior.AutoSize = true;
+            this.lblLimiteSuperior.ForeColor = System.Drawing.Color.White;
+            this.lblLimiteSuperior.Location = new System.Drawing.Point(275, 11);
+            this.lblLimiteSuperior.Name = "lblLimiteSuperior";
+            this.lblLimiteSuperior.Size = new System.Drawing.Size(127, 21);
+            this.lblLimiteSuperior.TabIndex = 2;
+            this.lblLimiteSuperior.Text = "Límite superior: ";
+            // 
+            // txtLimiteSuperior
+            // 
+            this.txtLimiteSuperior.Location = new System.Drawing.Point(408, 8);
+            this.txtLimiteSuperior.MaxLength = 10;
+            this.txtLimiteSuperior.Name = "txtLimiteSuperior";
+            this.txtLimiteSuperior.Size = new System.Drawing.Size(100, 27);
+            this.txtLimiteSuperior.TabIndex = 3;
+            this.txtLimiteSuperior.TextChanged += new System.EventHandler(this.txtLimiteSuperior_TextChanged);
             // 
             // frmSumaDeMatrices
             // 
@@ -172,7 +250,7 @@
             this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.Black;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "frmSumaDeMatrices";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Load += new System.EventHandler(this.frmSumaDeMatrices_Load);
@@ -180,9 +258,12 @@
             this.pnlTitulo.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbxCerrar)).EndInit();
             this.pnlContenido.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dgvMatriz1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvMatriz2)).EndInit();
+            this.pnlContenido.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvMatrizResultado)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMatriz2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMatriz1)).EndInit();
+            this.pnlCampos.ResumeLayout(false);
+            this.pnlCampos.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -198,5 +279,11 @@
         private System.Windows.Forms.DataGridView dgvMatriz1;
         private System.Windows.Forms.DataGridView dgvMatrizResultado;
         private System.Windows.Forms.DataGridView dgvMatriz2;
+        private System.Windows.Forms.Label lblMas;
+        private System.Windows.Forms.Panel pnlCampos;
+        private System.Windows.Forms.TextBox txtLimiteSuperior;
+        private System.Windows.Forms.Label lblLimiteSuperior;
+        private System.Windows.Forms.TextBox txtLimiteInferior;
+        private System.Windows.Forms.Label lblValor1;
     }
 }
--- Estructura_De_Datos_Proyecto/Estructura_De_Datos_Proyecto/frmSumaDeMatrices.cs
+++ Estructura_De_Datos_Proyecto/Estructura_De_Datos_Proyecto/frmSumaDeMatrices.cs
@@ -26,17 +26,74 @@ namespace Estructura_De_Datos_Proyecto
 
         private void GenerarRenglonesYColumnas()
         {
-            for (int i = 0; i < col; i++)
+            try
             {
-                dgvMatriz1.Columns.Add("Column" + i, "Columna " + i);
-                dgvMatriz2.Columns.Add("Column" + i, "Columna " + i);
-                dgvMatrizResultado.Columns.Add("Column " + i, "Columna " + i);
+                dgvMatriz1.Columns.Clear();
+                dgvMatriz2.Columns.Clear();
+                for (int i = 0; i < col; i++)
+                {
+                    dgvMatriz1.Columns.Add("Column" + i, "Columna " + i);
+                    dgvMatriz2.Columns.Add("Column" + i, "Columna " + i);
+                    dgvMatrizResultado.Columns.Add("Column " + i, "Columna " + i);
+                }
+                for (int i = 0; i < ren; i++)
+                {
+                    dgvMatriz1.Rows.Add();
+                    dgvMatriz2.Rows.Add();
+                    dgvMatrizResultado.Rows.Add();
+                }
             }
-            for (int i = 0; i < ren; i++)
+            catch (Exception ex)
             {
-                dgvMatriz1.Rows.Add();
-                dgvMatriz2.Rows.Add();
-                dgvMatrizResultado.Rows.Add();
+                MessageBox.Show("Error general: " + ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+            }
+        }
+
+        private void Random()
+        {
+            try
+            {
+                int ran = 0;
+                Random random = new Random();
+                if (Convert.ToInt32(txtLimiteInferior.Text) > Convert.ToInt32(txtLimiteSuperior.Text))
+                {
+                    MessageBox.Show("El límite inferior no puede ser mayor al límite superior", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                }
+                else
+                {
+                    for (int i = 0; i < ren; i++)
+                    {
+                        ran = random.Next(Convert.ToInt32(txtLimiteInferior.Text), Convert.ToInt32(txtLimiteSuperior.Text));
+                        for (int j = 0; j < col; j++)
+                        {
+                            dgvMatriz1.Rows[i].Cells[j].Value = ran.ToString();
+                        }
+                    }
+                    for (int i = 0; i < ren; i++)
+                    {
+                        ran = random.Next(Convert.ToInt32(txtLimiteInferior.Text), Convert.ToInt32(txtLimiteSuperior.Text));
+                        for (int j = 0; j < col; j++)
+                        {
+                            dgvMatriz2.Rows[i].Cells[j].Value = ran.ToString();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error general: " + ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+            }
+        }
+
+        private void SumarMatrices()
+        {
+            //int num = 0;
+            for (int i = 0; i < dgvMatriz1.ColumnCount; i++)
+            {
+                for (int j = 0; j < dgvMatriz1.Rows.Count; j++)
+                {
+                    dgvMatrizResultado.Rows[j].Cells[i].Value = (Convert.ToInt32(dgvMatriz1.Rows[j].Cells[i].Value.ToString()) + Convert.ToInt32(dgvMatriz2.Rows[j].Cells[i].Value.ToString()));
+                }
             }
         }
 
@@ -51,6 +108,47 @@ namespace Estructura_De_Datos_Proyecto
         private void pbxCerrar_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private void btnRandom_Click(object sender, EventArgs e)
+        {
+            Random();
+        }
+
+        private void txtLimiteInferior_TextChanged(object sender, EventArgs e)
+        {
+            if (!RegularExpressions.NumerosEnteros(txtLimiteInferior.Text))
+            {
+                txtLimiteInferior.Clear();
+                txtLimiteInferior.Focus();
+            }
+        }
+
+        private void txtLimiteSuperior_TextChanged(object sender, EventArgs e)
+        {
+            if (!RegularExpressions.NumerosEnteros(txtLimiteSuperior.Text))
+            {
+                txtLimiteSuperior.Clear();
+                txtLimiteSuperior.Focus();
+            }
+        }
+
+        private void dgvMatriz2_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dgvMatriz2_CellLeave(object sender, DataGridViewCellEventArgs e)
+        {
+            //if (!RegularExpressions.NumerosEnteros(dgvMatriz1.CurrentRow.Cells[e.RowIndex].Value.ToString()))
+            //{
+            //    MessageBox.Show("Mierda");
+            //}
+        }
+
+        private void btnSuma_Click(object sender, EventArgs e)
+        {
+            SumarMatrices();
         }
     }
 }
